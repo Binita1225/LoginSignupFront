c8511e2f3e7970d6d409e5008385cd089c2d0cf9
Binita1225 <Binita1225@users.noreply.github.com>
2025-02-13 15:52:22 +0545

    api for contact form

---
+++ src/services/contactFormServices.tsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "https://localhost:7161/api/ContactForm";
+
+export const getContactForm = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const addContactForm = async (formData: any) => {
+  const response = await axios.post(API_URL, formData);
+  return response.data;
+};
